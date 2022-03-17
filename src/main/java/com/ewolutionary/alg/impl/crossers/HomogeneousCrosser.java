3711179e6b23eda69071b76d2f93b824b7e2f38d
Camillo-commits <kamil.osika@spoko.pl>
2022-03-17 17:56:44 +0100

    Added Homogeneous Crosser

--- src/main/java/com/ewolutionary/alg/impl/crossers/HomogeneousCrosser.java
+++ src/main/java/com/ewolutionary/alg/impl/crossers/HomogeneousCrosser.java
@@ -2,12 +2,49 @@ package com.ewolutionary.alg.impl.crossers;
 
 import com.ewolutionary.alg.impl.Entity;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class HomogeneousCrosser extends Crosser {
 
     @Override
     protected List<Entity> cross(Entity first, Entity second) {
-        return null;
+        int chromosomeLength = first.getChromosome().length;
+        byte[] template = generateTemplate(chromosomeLength);
+
+        byte[] firstNew = new byte[chromosomeLength];
+        byte[] secondNew = new byte[chromosomeLength];
+        byte[] firstBinary = first.getChromosome();
+        byte[] secondBinary = second.getChromosome();
+
+        for (int i = 0; i < chromosomeLength; ++i) {
+            if (template[i] == 0) {
+                firstNew[i] = firstBinary[i];
+                secondNew[i] = secondBinary[i];
+            } else if (template[i] == 1) {
+                firstNew[i] = secondBinary[i];
+                secondNew[i] = firstBinary[i];
+            }
+        }
+
+        return Arrays.asList(new Entity(first.getStart(), first.getStop(), firstNew),
+                new Entity(second.getStart(), second.getStop(), secondNew));
     }
+
+    private byte[] generateTemplate(int chromosomeLength) {
+        byte[] template = new byte[chromosomeLength];
+        for (int i = 0; i < chromosomeLength; ++i) {
+            template[i] = (byte) rnd.nextInt(2);
+        }
+        return template;
+    }
+
+    public HomogeneousCrosser() {
+    }
+
+    public HomogeneousCrosser(Random random) {
+        this.rnd = random;
+    }
+
 }

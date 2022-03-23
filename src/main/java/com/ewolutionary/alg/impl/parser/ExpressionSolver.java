189cbe2ce7714e517d1bd73709e26ba006430e2d
takikuba <jakub26@gmail.com>
2022-03-23 23:58:54 +0100

    Implement expression parser to solve functions

---
+++ src/main/java/com/ewolutionary/alg/impl/parser/ExpressionSolver.java
@@ -0,0 +1,75 @@
+package com.ewolutionary.alg.impl.parser;
+
+import com.bgsoftware.superiorskyblock.scripts.RhinoScript;
+import com.ewolutionary.alg.impl.Entity;
+
+import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpressionSolver {
+
+    private ExpressionSolver() {}
+
+    public static double solve(String expression, Entity entity) {
+        if(expression.indexOf("pow(")>expression.indexOf("sqrt(")){
+            while(expression.contains("pow")) {
+                expression = resolvePowerFunction(expression);
+            }
+            while(expression.contains("sqrt")) {
+                expression = resolveSqrtFunction(expression);
+            }
+        } else {
+            while(expression.contains("sqrt")) {
+                expression = resolveSqrtFunction(expression);
+            }
+            while(expression.contains("pow")) {
+                expression = resolvePowerFunction(expression);
+            }
+        }
+
+        RhinoScript rs = RhinoScript.getInstance();
+        String strResult;
+
+        try {
+            strResult = rs.eval(expression).toString();
+            return Double.parseDouble(strResult);
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+        return 0.0;
+    }
+
+    private static String resolvePowerFunction(String expression) {
+        int powIndex = expression.indexOf("pow(")+4;
+        int powEndIndex =  expression.indexOf(')', powIndex);
+        String str = expression.substring(powIndex, powEndIndex);
+        while(str.contains("sqrt")) {
+            str = resolveSqrtFunction(str);
+        }
+        List<Double> d = new ArrayList<>();
+        for(String s: str.split(",")) {
+            d.add(Double.parseDouble(s));
+        }
+        double power = Math.pow(d.get(0), d.get(1));
+        String powerReplace = expression.substring(powIndex-4, powEndIndex+1);
+
+        return expression.replace(powerReplace, String.valueOf(power));
+    }
+
+    private static String resolveSqrtFunction(String expression) {
+        int startIndex = expression.indexOf("sqrt(")+5;
+        int endIndex =  expression.indexOf(')', startIndex);
+        String ss = expression.substring(startIndex, endIndex);
+        while(ss.contains("pow")) {
+            ss = resolvePowerFunction(ss);
+        }
+        double doub = Double.parseDouble(ss);
+        double power = Math.sqrt(doub);
+        String powerReplace = expression.substring(startIndex-5, endIndex+1);
+
+        return expression.replace(powerReplace, String.valueOf(power));
+    }
+
+}
---
+++ src/test/java/com/ewolutionary/alg/impl/parser/ExpressionSolverTest.java
@@ -0,0 +1,36 @@
+package com.ewolutionary.alg.impl.parser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExpressionSolverTest {
+
+    @Test
+    public void shouldParsePowerExpression() {
+        assertEquals(9.0, ExpressionSolver.solve("pow(3,2)", null));
+        assertEquals(25.0, ExpressionSolver.solve("pow(3,2)+pow(2,4)", null));
+        assertEquals(1024.0, ExpressionSolver.solve("pow(2,10)", null));
+        assertEquals(1049.0, ExpressionSolver.solve("pow(2,10)+pow(3,2)+pow(2,4)", null));
+        assertEquals(35.0, ExpressionSolver.solve("pow(3,2)+pow(2,4)+2*5", null));
+    }
+
+    @Test
+    public void shouldParseSqrtExpression() {
+        assertEquals(2.0, ExpressionSolver.solve("sqrt(4)", null));
+        assertEquals(5.0, ExpressionSolver.solve("sqrt(4)+sqrt(9)", null));
+        assertEquals(15.0, ExpressionSolver.solve("sqrt(4)+2*5+sqrt(9)", null));
+        assertEquals(32.0, ExpressionSolver.solve("sqrt(1024)", null));
+    }
+
+    @Test
+    public void shouldParseSqrtWithPower() {
+        assertEquals(83.0, ExpressionSolver.solve("sqrt(4) + pow(3,4)", null));
+        assertEquals(4.0, ExpressionSolver.solve("sqrt(pow(2,4))", null));
+        assertEquals(4.0, ExpressionSolver.solve("pow(2,sqrt(4))", null));
+    }
+
+}

f26356352b34bf2ed7f2e2e8837230d44b1f3e35
DamianZamojski <damian.zam1f20@gmail.com>
2022-04-02 21:02:51 +0200

    TournamentSelector fix

--- src/main/java/com/ewolutionary/alg/views/View.java
+++ src/main/java/com/ewolutionary/alg/views/View.java
@@ -25,6 +25,7 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.NumberField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 

1c34a0c808236c1ff32a1849492a094816ac3f6e
takikuba <kubkli26@gmail.com>
2022-04-21 08:31:05 +0200

    Add GaussMutator

--- src/main/java/com/ewolutionary/alg/impl/mutators/GaussMutator.java
+++ src/main/java/com/ewolutionary/alg/impl/mutators/GaussMutator.java
@@ -1,12 +1,32 @@
 package com.ewolutionary.alg.impl.mutators;
 
+import com.ewolutionary.alg.impl.Chromosome;
 import com.ewolutionary.alg.impl.Entity;
 
 import java.util.List;
+import java.util.Random;
 
 public class GaussMutator implements Mutator {
+
+    private Random rnd;
+
+    public GaussMutator() {
+        this.rnd = new Random();
+    }
+
     @Override
-    public void mutate(List<Entity> individualism, double mutationProbability) {
+    public void mutate(List<Entity> individuals, double mutationProbability) {
+
+        double newValue;
+        for(Entity entity: individuals) {
+            for(Chromosome chromosome: entity.getChromosomes()) {
+                newValue = chromosome.getValue() + rnd.nextGaussian();
+                while(newValue < entity.getStart() || newValue > entity.getStop()) {
+                    newValue = chromosome.getValue() + rnd.nextGaussian();
+                }
+                chromosome.setValue(newValue);
+            }
+        }
 
     }
 }
